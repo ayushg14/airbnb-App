81e637d6bf7f88a096e3cb00b7613d104f0370aa
Ayush Gupta <ayushg1408@gmail.com>
2024-04-12 19:18:16 +0530

    Explore Page Custom Header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,25 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import React, { useMemo, useState } from "react";
+import { Link, Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listings from "@/components/Listings";
+import listingsData from "@/assets/data/airbnb-listings.json";
 
 const Page = () => {
+  const [category, setCategory] = useState("Tiny homes");
+  const items = useMemo(() => listingsData as any, []);
+  const onDataChanged = (category: string) => {
+    // console.log("CHANGED_ ", category);
+    setCategory(category);
+  };
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
-      <Link href={"/listing/1330"}>Listing details</Link>
+    <View style={{ flex: 1, marginTop: 100 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+      <Listings listings={items} category={category} />
     </View>
   );
 };
